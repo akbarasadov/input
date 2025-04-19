492d971a7af364bf45fd9cdbe9571fe8b286a9ff
akbarasadov <akbarasadov4170@gmail.com>
2025-04-19 08:42:35 +0500

    frist upload

--- js.js
+++ js.js
@@ -7,53 +7,101 @@ let text2 = document.querySelector(".text2")
 let text3 = document.querySelector(".text3")
 let text4 = document.querySelector(".text4")
 
-let error = document.querySelectorAll(".error")
 let required = form.querySelectorAll(".required")
-let notError = form.querySelectorAll(".notError")
 
-labels.forEach(label => {
-    label.classList.remove("error")
-})
+
 
 text1.textContent = "All: " + labels.length
 text2.textContent = "Need: " + required.length
-console.log(error);
+
+let error = 0
+let success = 0
+
+let p = document.querySelectorAll(".p")
+let erors = document.querySelectorAll(".eror")
+
+let name = document.querySelector("#name")
+let email = document.querySelector("#email")
+let age = document.querySelector("#age")
+let questions = document.querySelectorAll("#questions")
+
+let name1 = document.querySelector(".name")
+let email1 = document.querySelector(".email")
+let age1 = document.querySelector(".age")
+let questions1 = document.querySelectorAll(".questions")
 
 
 
+let regex = [
+    //name
+    /^[A-Za-z]+(?: [A-Za-z]+)*$/,
+    //email
+    /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gim,
+    //age
+    /^(?:1[01][0-9]|120|[1-9]?[0-9])$/,
+    //all
+    /^[\s\S]*$/,
+]
 
-button.onclick = (e) => {
-    e.preventDefault()
-    text3.textContent = "Success: 7/" + notError.length
-    text4.textContent = "Error: " + error.length + "/"  + required.length
-    const formData = {}
-    let isError = false
-    
-    labels.forEach(label => {
-        let input = label.querySelector("input")
-        let erorMassage = label.querySelector(".feel")
+let nametexterror = document.querySelector(".nametexterror")
+let emailtexterror = document.querySelector(".emailtexterror")
+let agetexterror = document.querySelector(".agetexterror")
+let questionstexterror = document.querySelector(".questionstexterror")
 
-        if (label.classList.contains("required")) {
-            if (input.value.length === 0) {
-                label.classList.add("error")
-                erorMassage.textContent = "Please enter " + input.name
-                isError = true
-                button.classList.add("err")
-                label.classList.remove("notError")
-            } else {
-                label.classList.remove("error")
-                erorMassage.textContent = "Need to fil"
-                button.classList.remove("err")
-                label.classList.add("notError")
-            }
+
+let values = [name, age, email, questions]
+let a = [name1, age1, email1, questions1]
+let errortext = [
+    nametexterror.querySelector(".nametext"),
+    emailtexterror.querySelector(".emailtext"),
+    agetexterror.querySelector(".agetext"),
+    questionstexterror.querySelector(".questionstext"),
+]
+
+button.onclick = () => {
+
+    formData = {}
+
+    error = 0
+    success = 0
+
+    values.forEach((input, idx) => {
+        let isValid = regex[idx].test(input.value);
+        let errorText = errortext[idx];
+
+        console.log(input);
+
+
+        if (input.value === "") {
+            input.classList.add("error")
+            input.classList.remove("notError")
+            errorText.textContent = "Please enter " + input.name
+            error++
+        } else if (isValid === false) {
+            input.classList.add("error")
+            input.classList.remove("notError")
+            errorText.textContent = `Invalid ${input.name}`
+            error++
+        } else {
+            errorText.textContent = `Good thank you`
+            formData[input.name] = input.value
+            success++
+            input.classList.add("notError")
+            input.classList.remove("error")
         }
-        formData[input.name] = input.value
-    })
-    if (isError === false) {
-        console.log(formData);
+    });
+
+
+    text3.textContent = "Success: " + required.length + "/" + (required.length - error)
+    text4.textContent = "Error: " + required.length + "/" + error
+
+    if (error === 0) {
         labels.forEach(label => {
             label.querySelector("input")
             label.value = ""
+            console.log(formData);
         })
+    } else {
+        button.classList.add("err")
     }
 }
